97392778c5d728628b6f05510b5e89a81c86889b
Muangkong Yang <myang5@eagles.ewu.edu>
2019-11-03 22:54:01 -0800

    CSCD
    cscd330 filetransfer

---
+++ CSCD330 - Computer Networks/filetransfer/src/FileTransferClient.java
@@ -0,0 +1,98 @@
+/********************
+ * @Muangkong Yang  *
+ * CSCD330          *
+ * FileTransfer     *
+ ********************/
+
+import java.io.*;
+import java.net.*;
+
+public class FileTransferClient 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		int port = 8080;
+		Socket cSocket = new Socket("localhost", port);
+		
+		try 
+		{
+			FileTransferCli fileTransfer = new FileTransferCli(cSocket);
+			new Thread(fileTransfer).start();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
+
+final class FileTransferCli implements Runnable
+{
+	Socket socket;
+	
+	public FileTransferCli(Socket socket)
+	{
+		this.socket = socket;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			recieveFile();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	private void recieveFile() throws IOException
+	{
+		int bytesToRead = 4000;
+		byte[] fileReader = new byte[bytesToRead];
+		int bytesRead;
+		InputStream is = null; 
+		
+		try
+		{
+			is = socket.getInputStream();
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			if(is != null)
+			{	
+				FileOutputStream fos = null;
+				BufferedOutputStream bos = null;
+				try
+				{
+					fos = new FileOutputStream("testFileTransfer.mp3");
+					bos = new BufferedOutputStream(fos);
+					bytesRead = is.read(fileReader, 0, fileReader.length);
+					System.out.println("Starting to recieve file...");
+					do
+					{
+						System.out.println("(" + bytesRead + " bytes read/" + fileReader.length + " bytes)");
+						baos.write(fileReader, 0, bytesRead);
+						bytesRead = is.read(fileReader, 0, fileReader.length);
+					} while (bytesRead != -1);
+					
+					bos.write(baos.toByteArray());
+					System.out.println("Done recieving file!");
+					bos.flush();
+					bos.close();
+					socket.close();
+					
+					return;
+				}
+				catch(Exception e)
+				{
+					System.out.println(e);
+				}
+			}
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
---
+++ CSCD330 - Computer Networks/filetransfer/src/FileTransferServer.java
@@ -0,0 +1,87 @@
+/********************
+ * @Muangkong Yang  *
+ * CSCD330          *
+ * FileTransfer     *
+ ********************/
+
+import java.io.*;
+import java.net.*;
+
+public class FileTransferServer 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		int port = 8080;
+		ServerSocket sSocket = new ServerSocket(port);
+		
+		while(true)
+		{
+			Socket cSocket = sSocket.accept();
+			System.out.println("Connected to " + cSocket.getLocalAddress());
+			
+			FileTransfer fileTransfer = new FileTransfer(cSocket);
+			new Thread(fileTransfer).start();
+		}
+
+	}
+}
+
+final class FileTransfer implements Runnable
+{
+	Socket socket;
+	
+	public FileTransfer(Socket socket)
+	{
+		this.socket = socket;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			sendFile();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+	private void sendFile() throws IOException
+	{
+		File myFile = new File("C:\\Users\\Goten\\Desktop\\School\\CSCD 330 - Imamura\\filetransfer\\src\\Classical-guitar-music.mp3");
+		byte[] fileBuffer = new byte[(int) myFile.length()];
+		FileInputStream fis = null; 
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		
+		int bytesPerSend = 3000;
+		
+		try
+		{
+			fis = new FileInputStream(myFile);
+			bis = new BufferedInputStream(fis);
+			bos = new BufferedOutputStream(socket.getOutputStream());
+			System.out.println("Sending file..."); 
+			int bytesSent = bis.read(fileBuffer, 0, bytesPerSend);
+			int count = bytesSent;
+			int total = 0;
+			do
+			{
+				System.out.println("Sent " + total + "/" + fileBuffer.length + " bytes");
+				total += count;
+				bos.write(fileBuffer, 0, count);
+				
+			} while((count = bis.read(fileBuffer, 0, bytesPerSend)) > 0);
+			
+			System.out.println("Done sending file.");
+			bos.flush();
+			bos.close();
+			socket.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
